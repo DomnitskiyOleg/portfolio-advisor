74a06b08f346f6359fe89fb8cdb2de3129de8c9f
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-06 16:40:20 +0600

    add some testing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationMenu from './components/NavigationMenu';
+import LogIn from './components/LogIn';
+import Registration from './components/Registration';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
-     <NavigationMenu />
-      <div className='d-flex container-fluid h-100 justify-content-center align-items-center bg-light'></div>
+      <NavigationMenu />
+      <div className='d-flex container-fluid h-100 justify-content-center align-items-center bg-light'>
+        <Routes>
+          <Route path='/' element={null} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/registration' element={<Registration />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
