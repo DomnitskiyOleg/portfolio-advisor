b2d7d2979f65efddb9181983062eaa778f18592a
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-06 17:33:00 +0600

    fix font weight

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,41 +1,88 @@
 import React from 'react';
+import { gsap } from 'gsap';
+import { Link } from 'react-router-dom';
 import { Navbar, Container, Button, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 
-const NavigationMenu = () => (
-  <Navbar expand='lg' className='shadow-sm bg-body-wigth'>
-    <Container>
-      <Navbar.Brand href='#home'>
-        <img
-          alt=''
-          src={logo}
-          width='30'
-          height='30'
-          className='d-inline-block align-top'
-        />{' '}
-        Portfolio Advisor
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav '>
-        <Nav className='me-auto'>
-          <Nav.Link href='#home'>Главная</Nav.Link>
-          <Nav.Link href='#link'>Создать портфель</Nav.Link>
-          <Nav.Link href='#home'>Мой портфель</Nav.Link>
-          <Nav.Link href='#link' disabled>
-            Анализ портфеля
-          </Nav.Link>
-        </Nav>
-        <Nav>
-          <Button className='m-1' variant='outline-info'>
-            Регистрация
-          </Button>
-          <Button className='m-1' variant='outline-info'>
-            Войти
-          </Button>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+const NavigationMenu = () => {
+  const onEnter = ({ currentTarget }: { currentTarget: HTMLElement }): void => {
+    gsap.to(currentTarget, { backgroundColor: '#F2F2F2', scale: 1.07 });
+  };
+
+  const onLeave = ({ currentTarget }: { currentTarget: HTMLElement }): void => {
+    gsap.to(currentTarget, { backgroundColor: '#ffff', scale: 1 });
+  };
+  return (
+    <Navbar expand='lg' className='shadow-sm bg-body-wigth'>
+      <Container>
+        <Navbar.Brand
+          as={Link}
+          to='/'
+          className='rounded-3 m-1 p-2'
+          onMouseEnter={onEnter}
+          onMouseLeave={onLeave}
+        >
+          <img
+            alt=''
+            src={logo}
+            width='30'
+            height='30'
+            className='d-inline-block align-top'
+          />{' '}
+          Portfolio Advisor
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav '>
+          <Nav className='me-auto'>
+            <Nav.Link
+              href='#home'
+              className='rounded-3 m-1'
+              onMouseEnter={onEnter}
+              onMouseLeave={onLeave}
+            >
+              Главная
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to='/registration'
+              href='#create'
+              className='rounded-3 m-1'
+              onMouseEnter={onEnter}
+              onMouseLeave={onLeave}
+            >
+              Создать портфель
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to='/login'
+              className='rounded-3 m-1'
+              onMouseEnter={onEnter}
+              onMouseLeave={onLeave}
+            >
+              Мой портфель
+            </Nav.Link>
+            <Nav.Link
+              href='#link'
+              className='rounded-3 m-1'
+              onMouseEnter={onEnter}
+              onMouseLeave={onLeave}
+              disabled={true}
+            >
+              Анализ портфеля
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Button className='m-1' variant='outline-info'>
+              Регистрация
+            </Button>
+            <Button className='m-1' variant='outline-info'>
+              Войти
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavigationMenu;
