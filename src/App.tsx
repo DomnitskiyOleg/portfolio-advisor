5913101d7a0d636d8cc87651e0f665399423f2d1
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-07 16:12:19 +0600

    add private route logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,30 @@ import LogIn from './components/LogIn';
 import Registration from './components/Registration';
 import CreatePortfolio from './components/CreatePortfolio';
 import AuthProvider from './components/AuthProvider';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 const App = () => {
   return (
     <AuthProvider>
-       <Router>
-      <NavigationMenu />
-      <div className='d-flex container-fluid flex-grow-1 justify-content-center align-items-center bg-light'>
-        <Routes>
-          <Route path='/' element={null} />
-          <Route path='/login' element={<LogIn />} />
-          <Route path='/registration' element={<Registration />} />
-          <Route path='/create' element={<CreatePortfolio />} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <NavigationMenu />
+        <div className='d-flex container-fluid flex-grow-1 justify-content-center align-items-center bg-light'>
+          <Routes>
+            <Route path='/' element={null} />
+            <Route path='/login' element={<LogIn />} />
+            <Route path='/registration' element={<Registration />} />
+            <Route
+              path='/create'
+              element={
+                <PrivateRoute>
+                  <CreatePortfolio />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </AuthProvider>
   );
 };
