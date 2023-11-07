7ab0a472c3da0eb89eeb652551dc18463524b178
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-07 11:42:47 +0600

    add route /create

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationMenu from './components/NavigationMenu';
 import LogIn from './components/LogIn';
 import Registration from './components/Registration';
+import CreatePortfolio from './components/CreatePortfolio';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
       <NavigationMenu />
-      <div className='d-flex container-fluid h-100 justify-content-center align-items-center bg-light'>
+      <div className='d-flex container-fluid flex-grow-1 justify-content-center align-items-center bg-light'>
         <Routes>
           <Route path='/' element={null} />
           <Route path='/login' element={<LogIn />} />
           <Route path='/registration' element={<Registration />} />
+          <Route path='/create' element={<CreatePortfolio />} />
         </Routes>
       </div>
     </Router>
