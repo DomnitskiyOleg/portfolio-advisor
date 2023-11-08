099d459e12705c8e133cb8e86fd5aa83861c00f0
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-08 20:38:14 +0600

    add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,31 @@ export interface Auth {
   logIn: () => void;
   logOut: () => void;
 }
+
+// export interface UserAnswers {
+//   investGoal: string
+//   investTime: number;
+//   spendIncome: string;
+//   expectedProfit: string;
+//   investType: string;
+//   age: number;
+// }
+
+export interface UserAnswers {
+  investGoal: '' | 'ptotect' | 'medGrow' | 'grow';
+  investTime: number;
+  spendIncome: '' | 'spendAll' | 'spendPart' | 'reinvest';
+  expectedProfit: '' | 'earn5' | 'earn10' | 'earn15';
+  investType: '' | 'deposits' | 'bonds' | 'stocks';
+  age: number;
+}
+
+export type EtfType = 'SBGB' | 'SBGD' | 'SBRB' | 'SBMX' | 'SBHI';
+
+export interface EtfRatesType {
+  SBGB?: number;
+  SBGD?: number;
+  SBRB?: number;
+  SBMX?: number;
+  SBHI?: number;
+}
