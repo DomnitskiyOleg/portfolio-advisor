3bfce1c0ab52a9560d9767f01463ee0090d73e4e
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-08 20:36:08 +0600

    add table comp

---
+++ src/components/EtfTable.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import type { EtfRatesType } from '../types/index';
+
+const EtfTable = ({ etfAllocation }: { etfAllocation: EtfRatesType }) => {
+  const etfList = Object.entries(etfAllocation);
+  const rows = etfList.map(([ticker, price]) => (
+    <tr>
+      <td>{ticker}</td>
+      <td>{price}</td>
+    </tr>
+  ));
+  return (
+    <Table className='table-sm table-info' bordered hover>
+      <thead>
+        <tr>
+          <th>
+            <span className='text-muted'>Тикер бумаги</span>
+          </th>
+          <th>
+            <span className='text-muted'>Количество</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
+  );
+};
+
+export default EtfTable;
