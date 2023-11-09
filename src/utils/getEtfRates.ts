57f0e86fc824d3e7982367a36c5996dca9378ca9
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-09 15:21:05 +0600

    refactoring utility getrates

--- src/utils/getEtfRates.ts
+++ src/utils/getEtfRates.ts
@@ -5,7 +5,13 @@ import type { EtfType, EtfRatesType } from '../types/index';
 const getEtfData = async (etfs: EtfType[]) => {
   const promises = etfs.map((etf) => axios.get(getMoexUrl(etf)));
   const responses = await Promise.all(promises);
-  const initialAcc: EtfRatesType = {};
+  const initialAcc: EtfRatesType = {
+    SBGB: 0,
+    SBGD: 0,
+    SBRB: 0,
+    SBMX: 0,
+    SBHI: 0,
+  };
   const etfRates = responses.reduce((acc, item) => {
     const [[ticker, value]]: [[EtfType, number]] = item.data.marketdata.data;
     acc[ticker] = value;
