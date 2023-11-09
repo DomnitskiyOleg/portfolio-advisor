a974579f9ae6855fdd51ef18783b80fcde04b87e
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-11-09 15:20:16 +0600

    add default strategy

--- src/static/strategies.ts
+++ src/static/strategies.ts
@@ -1,4 +1,4 @@
-import type { EtfType } from '../types/index';
+import type { EtfType, EtfPool } from '../types/index';
 
 export const strategies = {
   conservative: {
@@ -16,7 +16,22 @@ export const strategies = {
     облигации: { value: 20, name: 'облигации' },
     золото: { value: 10, name: 'золото' },
   },
-  none: {},
+  none: {
+    акции: { value: 0, name: 'акции' },
+    облигации: { value: 0, name: 'облигации' },
+    золото: { value: 0, name: 'золото' },
+  },
 };
 
 export const etfs: EtfType[] = ['SBGB', 'SBGD', 'SBRB', 'SBMX', 'SBHI'];
+
+
+export const etfPool: EtfPool = {
+  SBGB: { type: 'облигации', fraction: 2 },
+  SBGD: { type: 'золото', fraction: 1 },
+  SBRB: { type: 'облигации', fraction: 2},
+  SBMX: { type: 'акции', fraction: 2 },
+  SBHI: { type: 'акции', fraction: 2 },
+};
+
+
